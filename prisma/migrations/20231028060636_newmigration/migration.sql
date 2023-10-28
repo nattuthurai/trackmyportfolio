4149a5a27d311edd74fc9c8b07fbfec1412dea74
nattuthurai <nattuthurai.k@live.com>
2023-10-28 11:38:34 +0530

    schema created

---
+++ prisma/migrations/20231028060636_newmigration/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `UserDetails` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
